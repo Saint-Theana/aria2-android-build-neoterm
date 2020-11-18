818dbff55e6a25cc25c811167f8ddcaba38c28b1
Saint-Theana <Saint-Theana@github.com>
2020-11-18 23:40:36 +0800

    init

--- confdefs.h
+++
@@ -1,9 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "aria2"
-#define PACKAGE_TARNAME "aria2"
-#define PACKAGE_VERSION "1.35.0"
-#define PACKAGE_STRING "aria2 1.35.0"
-#define PACKAGE_BUGREPORT "https://github.com/aria2/aria2/issues"
-#define PACKAGE_URL "https://aria2.github.io/"
-#define PACKAGE "aria2"
-#define VERSION "1.35.0"
